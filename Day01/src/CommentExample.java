289e891eb465eecfdecb9a7c992ee409d145f2fa
jdsaeyqo <jdsaeyqi@naver.com>
2020-08-25 13:06:28 +0900

    1일차 자바 코드

---
+++ Day01/src/CommentExample.java
@@ -0,0 +1,52 @@
+/**
+ * 이 주석은 API문서 생성용 주석입니다.
+ * 개발자가 만든 코드를 API화 시킬 때
+ * 코드의 라이센스정보 및 설명을 첨언하는 
+ * 용도의 주석입니다.
+ * 
+ * @author Hong
+ * @version 1.0.0
+ * @since 2018.09.11~
+ *
+ */
+
+
+public class CommentExample {
+
+	public static void main(String[] args) {
+		/*
+		main함수는 프로그램을 실행하기 위해 
+		반드시 작성해야 하는 함수입니다.
+		
+		main함수 블록({})안에 들어있는 코드의 명령을
+		실행합니다.
+		
+		main함수를 쉽게 작성하시려면 이클립스 
+		코드작성부에 main이라고 입력하고
+		Ctrl + Space를 누르면 자동완성기능으로
+		작성할 수 있습니다.
+		*/
+		
+		//코드 실행 단축키는 Ctrl + F11입니다.
+		/*
+		 * println()은 print()와는 달리 출력 이후에
+		 * 자동으로 줄 개행을 합니다.(\n자동포함)
+		 */
+		//System.out.println("Hello~~!");
+		System.out.println("Bye!!");
+		/*
+		 * sysout이라고 적고 Ctrl + Space를 누르면
+		 * println()코드가 자동작성됩니다.
+		 */
+		System.out.println("방가방가~~!");
+		
+		System.out.printf("%d + %d = %d", 10, 3, 13);
+	}
+}
+
+
+
+
+
+
+
---
+++ Day01/src/HelloJava.java
@@ -0,0 +1,9 @@
+
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("안녕??\n");
+		System.out.print("반가워!!");
+	}
+}
